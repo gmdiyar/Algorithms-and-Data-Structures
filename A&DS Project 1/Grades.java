f1d98fb312c09022c637ac518bb834a61607674f
Diyar Giyash <dgiyashmail@gmail.com>
2026-09-05 22:16:26 -0500

    final

--- A&DS Project 1/Grades.java
+++ A&DS Project 1/Grades.java
@@ -1,52 +1,75 @@
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Scanner;
 
 public class Grades {
 
     ArrayList<String> students = new ArrayList<String>();
-    
-    public static int getStudentCount(){
+
+    public static int getStudentCount() {
         Scanner input = new Scanner(System.in);
         int numberOfStudents;
 
         System.out.println("Please enter number of students: ");
         while (true) {
-            if (input.hasNextInt()){
+            if (input.hasNextInt()) {
                 numberOfStudents = input.nextInt();
                 break;
             } else {
                 System.out.println("Please enter an integer: ");
                 input.nextLine();
             }
-        } return numberOfStudents;
+        }
+        return numberOfStudents;
     }
-    
-    public static void populateStudentArray(){
-        
-        int studentCount = getStudentCount();
-        
-        Scanner input = new Scanner(System.in);
-        System.out.println("Enter " + studentCount + " scores (seperated by spaces): ");
 
-        while (true){
+    public static int validateGrades(ArrayList<Float> scores) {
 
-            float line = input.nextFloat(); 
-            float[] floats;
-            int floatCount = floats.length;
+        float best = Collections.max(scores);
 
-            if (floatCount != studentCount) {
-                System.out.println("The number of scores you entered is not equal to the student count or the scores are not in the correct format (float)");
-                continue;
-            } else if (floatCount == studentCount) {
-                for (int i = 0; i < floats.length; i++){
-                    System.out.println(floats[i]);
-                }
-                }
+        int index = 0;
+
+        for (float score : scores) {
+            if (score >= best - 10) {
+                System.out.println("Student " + index + " score is " + score + " and grade is A");
+                index++;
+            } else if (score >= best - 20) {
+                System.out.println("Student " + index + " score is " + score + " and grade is B");
+                index++;
+            } else if (score >= best - 30) {
+                System.out.println("Student " + index + " score is " + score + " and grade is C");
+                index++;
+            } else if (score >= best - 40) {
+                System.out.println("Student " + index + " score is " + score + " and grade is D");
+                index++;
+            } else {
+                System.out.println("Student " + index + " score is " + score + " and grade is F");
+                index++;
             }
-        }      
+        }
+        return 0;
+
+    }
+
+    public static void getStudentArray() {
+        Scanner input = new Scanner(System.in);
+        ArrayList<Float> scores = new ArrayList<>();
+        int studentCount = getStudentCount();
+
+        System.out.println("Enter " + studentCount + " scores. (extra scores will be discarded.)");
+        for (int i = 0; i < studentCount; i++) {
+            while (!input.hasNextFloat()) {
+                System.out.println("Please enter only floats.");
+                input.next();
+            }
+            scores.add(input.nextFloat());
+        }
+
+        validateGrades(scores);
+
     }
 
     public static void main(String[] args) {
-        populateStudentArray();
+        getStudentArray();
     }
 }
